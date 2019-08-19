595a6fda8440fb49b92cfcc3ca611b2b6ed14ade
MrT3313 <rturgeon13@gmail.com>
2019-08-19 15:09:10 -0500

    arrow function conversion assignemnt

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,39 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+
+}
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => {
+    return param
+}
+console.log(anotherFunction("Hello"))
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (a,b) => {
+    return a + b
+}
+console.log(add(2,3))
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (a,b) => {
+    return a - b
+}
+console.log(subtract(3,5))
 
 // Stretch
 
@@ -28,3 +46,11 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+let triple = (array) => {
+    let newArray = array.map( item => {
+        return item * 3
+    })
+return newArray}
+
+console.log(triple([1,2,3,4]))
